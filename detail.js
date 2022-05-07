14a9c2b1833a37788f4eaaf4651b942fb448ec57
yyf9511 <66350676+yyf9511@users.noreply.github.com>
2022-05-07 15:45:19 +1200

    complete detail page

---
+++ detail.js
@@ -0,0 +1,40 @@
+randerDetailData()
+
+function randerDetailData() {
+  // get id from url
+  const url = new URL(window.location)
+  var params = new URLSearchParams(url.search.substring(1))
+  let getId = parseInt(params.get("prodId"), 10)
+
+  //
+  var selectData = rawdata.find(item => item.prodId == getId)
+  // console.log(getId)
+  // console.log(index)
+  title = selectData.title
+  price = selectData.price
+  descrip = selectData.description
+  productUrl = `https://storage.googleapis.com/luxe_media/wwwroot/${selectData.productMedia[0].url}`
+  const detailTemplate = `
+  <div class="detailTitle mgTop30">
+  ${title}
+  </div>
+
+  <hr />
+
+  <div class="row">
+    <div class="img-box col">
+      <img src="${productUrl}" alt="" class="img-size">
+    </div>
+    <div class="col">
+      <div>
+        ${descrip}
+      </div>
+      <div class="price-txt mgTop30">
+        $${price}
+      </div>
+    </div>
+  </div>
+  <input class="returnBt mgTop30" name="action" onclick="history.back()" type="submit" value="OK"/>
+      `
+  document.getElementById('detail').innerHTML = detailTemplate
+}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 randerData()
+createCateType()
 
 function randerData() {
   let randerTemplate = '' 
@@ -6,14 +7,16 @@ function randerData() {
     if(product.productMedia[0]) {
       title = product.title
       price = product.price
+      id = product.prodId
+      urlparam = `./detail.html?prodId=${id}`
       productUrl = `https://storage.googleapis.com/luxe_media/wwwroot/${product.productMedia[0].url}`
       const viewTemplate = `
-      <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
-          <a href="http://" class="img-Link">
+      <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 mgTop30">
+          <a href="${urlparam}" class="img-Link">
             <div class="img-box">
               <img src="${productUrl}" alt="" class="img-size">
             </div>
-            <div class="Template-txtBox row align-items-center">
+            <div class="mgTop30 row align-items-center">
               <div class="col" >
                 ${title}
               </div>
@@ -27,6 +30,42 @@ function randerData() {
       randerTemplate += viewTemplate
     }
   }
-  console.log(title)
   document.getElementById('category').innerHTML = randerTemplate
+}
+
+
+function createCateType() {
+  let OptionList = ''
+  let list = ''
+  for (let product of rawdata) {
+      if(product.prodType.typeName == "Hire") {
+        const productCategories = product.prodType.productCategory
+        productCategory = productCategories
+      }
+  }
+  for (const categoryTyprName of productCategory) {
+    const categoryId = categoryTyprName.categoryId
+    const categoryName = categoryTyprName.categoryName
+    const categoryOptionList = `
+      <option value="${categoryId}">${categoryName}</option>
+    `
+    OptionList += categoryOptionList
+    const allCate = `
+    <option selected>All Category</option>
+    `
+
+    list = allCate + OptionList
+  }
+  document.getElementById('categoryTypelist').innerHTML = list
+}
+
+function getcategType(selectObject) {
+  let value = selectObject.value
+  console.log(value)
+
+
+  // let data = ''
+  // let newdata = ''
+  // newdata = data.push(item => item.categoryId == value)
+  // console.log(newdata)
 }
