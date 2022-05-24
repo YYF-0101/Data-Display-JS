e02c4d9b6f316ba94218a312e1b0377a58dbe15a
yyf9511 <66350676+yyf9511@users.noreply.github.com>
2022-05-24 20:01:36 +1200

    refactor index

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
- 
-// spliteData(rawdata)
-randerData(rawdata)
-// randerPagination(rawdata)
-// eventListener()
-
 let state = []
 
 onInit()
@@ -11,23 +5,32 @@ onInit()
 function onInit() {
   createCateType()
   const webUrl = window.location.search.substring(1)
-  console.log(webUrl)
+  console.log(window.location.search.substring(1))
+   webUrl ? urlHandler(webUrl) : randerData(rawdata)
 }
 
-
-let cateType = ''
-let priceMin = 0
-let priceMax = 500000
-
-let resultArr = ''
+function urlHandler (params) {
+  searchParams = new URLSearchParams(params)
+  const urlCateId = searchParams.get ("category") ? searchParams.get ("category") : null
+  const urlPriceRange = searchParams.get ("priceRange") ? searchParams.get ("priceRange") : null
+  const urlSort = searchParams.get ("sort") ? searchParams.get ("sort") : null
+  urlCateId && (
+    urlRander('category',urlCateId)
+  )
+  urlPriceRange && (
+    urlRander('priceRange',urlPriceRange)
+  )
+  urlSort && (
+    urlRander('sort',urlSort)
+  )
+}
 
 function createCateType() {
   let OptionList = ''
   let list = ''
   for (let product of rawdata) {
       if(product.prodType.typeName == "Hire") {
-        const productCategories = product.prodType.productCategory
-        productCategory = productCategories
+        productCategory = product.prodType.productCategory
       }
   }
   for (const categoryTyprName of productCategory) {
@@ -38,7 +41,7 @@ function createCateType() {
     `
     OptionList += categoryOptionList
     const allCate = `
-    <option selected value="0">All Category</option>
+    <option value="0">All Category</option>
     `
 
     list = allCate + OptionList
@@ -78,129 +81,99 @@ function randerData(data) {
   document.getElementById('category').innerHTML = randerTemplate
 }
 
-function handlerCateType(params) {
-  cateType = parseInt(params.value)
-  filter()
-}
-
-function handlerPrice(params) {
-
-  switch (params.value) {
-      case "0":
-      priceMin = 0
-      priceMax = 50000
-      console.log(priceMax)
-      break;
-    
-      case "100":
-      priceMin = 0
-      priceMax = 100
-      console.log(priceMax)
-      break;
-
-      case "500":
-      priceMin = 101
-      priceMax = 500
-      console.log(priceMax)
-      break;
-
-      case "1000":
-      priceMin = 501
-      priceMax = 1000
-      console.log(priceMax)
-      break;
-
-      case "1001":
-      priceMin = 1001 
-      priceMax = 50000
-      console.log(priceMax)
-      break;
+function handler(key,value) {
+  if (key === 'category') {
+    const cateId = value
+    categoryFilter(cateId)
+    setUrlSearchParam(key,value)
+  }
+  if (key === 'priceRange') {
+    const priceRangeValue = value
+    priceRangeFilter(value)
+    setUrlSearchParam(key,value)
+  }
+  if (key === 'sort') {
+    console.log(key + value)
+    if (value === 'Ascending') {
+      console.log(key + value)
+      ascending(value)
+      setUrlSearchParam(key,value)
+    }
+    if (value === 'Decending') {
+      console.log(key + value)
+      decending(value)
+      setUrlSearchParam(key,value)
+    }
+  }
+  if (key === 'reset') {
+    reset()
+    delUrlSearchParam(key, value)
+  }
   
-    default:
-      break;
-  }
-  filter()
 }
 
-function filter() {
-  let filter = {
-    "categoryId": cateType,
-  }
-
-  // let tempFilter = {};
-  // for (key in filter) {
-  //   if (typeof(filter[key]) != "undefined" && typeof(filter[key] != "null" && filter[key] != null && filter[key] != '')) {
-  //     tempFilter[key] = filter[key];
-  //   }
-  // }
-
-  resultArr = rawdata.filter(
-    (item) => {
-      let flag = false
-      for (let key in filter) {
-        if (item.categoryId && item.productMedia[0]) {
-          if (item[key].toString().split(",").indexOf(cateType.toString()) >= 0  && item.price >= priceMin && item.price <= priceMax) {
-            flag = true
-          }else if (!cateType) {
-            if (item.price >= priceMin && item.price <= priceMax) {
-              flag = true
-            }
-          }else {
-            flag = false
-            break
-          }
-        }
-      }
-      if(flag) {
-        return item
+function categoryFilter(cateId) {
+    state = []
+    console.log(cateId)
+    for (const data of rawdata) {
+      if (data.categoryId === parseInt(cateId)) {
+        state.push(data)
       }
     }
-  )
-  pager(resultArr)
+    console.log(state)
+    randerData(state)
 }
 
-let ascendbtn = document.getElementById('ascendClick')
-ascendbtn.addEventListener("click",sortByAscend)
-let decendbtn = document.getElementById('decendClick')
-decendbtn.addEventListener("click",sortByDecend)
-let resetbtn = document.getElementById('resetClick')
-resetbtn.addEventListener("click",reset)
-
-function sortByAscend() {
-  ascendbtn.classList.add("disabled")
-  decendbtn.classList.remove("disabled")
-  if (resultArr) {
-    resultSortByA = resultArr.sort(function (a,b) {
-      return a.price - b.price
-    })
-  } else {
-    resultSortByA = rawdata.sort(function (a,b) {
-      return a.price - b.price
-    })
+function priceRangeFilter(priceRange) {
+  console.log(priceRange)
+  console.log(state)
+  let priceRangeState = []
+  for (const data of state) {
+    if (priceRange == 0) {
+      priceRangeState = state
+    }
+    if (priceRange == 100 && data.price > 0 && data.price < 100 ) {        
+      priceRangeState.push(data)
+    }
   }
-  console.log(resultSortByA)
+  console.log(priceRangeState)
+  randerData(priceRangeState)
+}
+
+function ascending(value) {
+  if (value === 'Ascending' && state != '') {
+        resultSortByA = state.sort(function (a,b) {
+          return a.price - b.price
+        })
+      } else {
+        resultSortByA = rawdata.sort(function (a,b) {
+          return a.price - b.price
+        })
+      }
+  document.getElementById('ascendClick').setAttribute("disabled","")
+  document.getElementById('decendClick').removeAttribute("disabled","")
   randerData(resultSortByA)
 }
-function sortByDecend() {
-  decendbtn.classList.add("disabled")
-  ascendbtn.classList.remove("disabled")
-  if (resultArr) {
-    resultSortByD = resultArr.sort(function (a,b) {
-      return b.price - a.price
-    })
-  } else {
-    resultSortByD = rawdata.sort(function (a,b) {
-      return b.price - a.price
-    })
-  }
-  console.log(resultSortByD)
+
+function decending(value) {
+  if (value === 'Decending' && state != '') {
+        resultSortByD = state.sort(function (a,b) {
+          return b.price - a.price
+        })
+      } else {
+        console.log("here")
+        resultSortByD = rawdata.sort(function (a,b) {
+          return b.price - a.price
+        })
+      }
+  document.getElementById('decendClick').setAttribute("disabled","")
+  document.getElementById('ascendClick').removeAttribute("disabled","")
   randerData(resultSortByD)
 }
+
 function reset() {
-  ascendbtn.classList.remove("disabled")
-  decendbtn.classList.remove("disabled")
-  if (resultArr) {
-    resultReset = resultArr.sort(function (a,b) {
+  if (state) {
+    resultReset = state.sort(function (a,b) {
       return a.prodId - b.prodId
     })
   } else {
@@ -208,88 +181,48 @@ function reset() {
       return a.prodId - b.prodId
     })
   }
-  console.log(resultReset)
   randerData(resultReset)
 }
 
-
-  // document.getElementById("previous").addEventListener("click", previous);
-  // document.getElementById("next").addEventListener("click", next);
-  // let obutton = document.getElementById("page").getElementsByTagName("li")
-  // obutton.addEventListener("click",currentPage)
-  // console.log(obutton)
-
-function previous() {
-  console.log("im previous")
+function setUrlSearchParam(key,value) {
+  const url = new URL(window.location.href)
+  url.searchParams.set(key, value)
+  window.history.pushState({ path: url.href }, '', url.href)
 }
 
-function next() {
-  console.log("im next")
+function delUrlSearchParam() {
+  const url = new URL(window.location.href)
+  url.searchParams.delete('category')
+  url.searchParams.delete('priceRange')
+  url.searchParams.delete('sort')
+  window.history.pushState({ path: url.href }, '', url.href)
+  document.getElementById('categoryTypelist').value = 0
+  document.getElementById('price').value = 0
+  randerData(rawdata)
 }
 
-// function init(){
-//   document.getElementById("next").onclick = function() { 
-//     pager("next");
-//     return false;
-//   };
-//   document.getElementById("previous").onclick = function() { 
-//     pager("previous"); 
-//     return false;
-//   };
-// }
-
-function pager() {
-  let pageSize = 8
-  let pageIndex = 0
-  let randerPage = ''
-  let currentPage = 0
-  
-  var pageNumbers= Math.ceil(resultArr.length/pageSize)
-
-  console.log(pageNumbers)
-
-  while (pageIndex < pageNumbers) {
-    pageIndex ++
-    const pageIndexTemplate = `
-    <li class="page-item"><a class="page-link" href="#">${pageIndex}</a></li>
-  `
-    randerPage += pageIndexTemplate
+function urlRander(key,value) {
+  if (key === 'category') {
+    const cateId = value
+    categoryFilter(cateId)
+    document.getElementById('categoryTypelist').value = cateId
   }
-
-  document.getElementById('page').innerHTML = randerPage
-  resultArr = Array
-    .from(Array(Math.ceil(resultArr.length / pageSize)))
-    .map((_, i) => resultArr.slice(i * pageSize, (i + 1) * pageSize));
-  
-    console.log(resultArr)
-  let ArrNumber = resultArr.length
-
-
-
-  if (ArrNumber == pageNumbers) {
-    console.log(resultArr)
-    randerData(resultArr?.[0])
-    // resultArr =resultArr[currentPage]
+  if (key === 'priceRange') {
+    const priceRangeValue = value
+    priceRangeFilter(value)
+    document.getElementById('price').value = priceRangeValue
+  }
+  if (key === 'sort') {
+    console.log(key + value)
+    if (value === 'Ascending') {
+      console.log(key + value)
+      ascending(value)
+      document.getElementById('ascendClick').setAttribute("disabled","")
+    }
+    if (value === 'Decending') {
+      console.log(key + value)
+      decending(value)
+      document.getElementById('decendClick').setAttribute("disabled","")
+    }
   }
 }
-
-// function pager(action, page) {
-//   switch (action) {
-//     case "next":
-//       if( (page + 1) < pageNumbers ){ 
-//         ++page;
-//         console.log(page)
-//       }
-//       console.log(page)
-//       break;
-     
-//     case "previous":
-//       if( (page - 1) >= 1 ){
-//          --page;
-//       }
-//       break;
-
-//     default:
-//       break;
-//   }
-// }
